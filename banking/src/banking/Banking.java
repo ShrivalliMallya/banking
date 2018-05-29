70e35d5301f04c37330582c00a26c5dfd3c6892a
SHREE SHANTERI <SHREE SHANTERI@SHREESHANTERI>
2018-05-29 15:48:31 +0530

    recently committed

--- banking/src/banking/Banking.java
+++ banking/src/banking/Banking.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package banking;
 
-/**
- *
- * @author SHREE SHANTERI
- */
+package banking;
+import java.util.Scanner;
+
 public class Banking {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner inp= new Scanner(System.in);
+        System.out.println("Enter 1.create 2.deposit 3.withdraw 4.Transfer 5.total amount 6.richest");
+        System.out.println("Enter the choice:");
+        int ch= inp.nextInt();
+        int i;
+        
+        
+        switch(ch)
+        {
+            case 1: {
+                        System.out.println("Enter the account number:");
+                        int acc= inp.nextInt();
+                        for(i=0; i<count; i++)
+                        {
+                            if(a[i].acc_no==acc)
+                            {
+                                System.out.println("Account already exists!");
+                                break;
+                            }
+                        }
+                        
+                        if(i==count)
+                            Account a[]
+                            
+                        
+                        
+                        
+                
+                    }
+        }
+        
+        
+        
     }
     
 }
